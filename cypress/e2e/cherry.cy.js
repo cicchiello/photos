d459a1fd6f0199581dc281c3e13edc8d2c6f6302
JFC <j.cicchiello@ieee.org>
2024-12-27 11:56:37 -0500

    - tagList exclusion spec is transfered between pages now - testing confirms tagList exclusion working

--- cypress/e2e/cherry.cy.js
+++ cypress/e2e/cherry.cy.js
@@ -1,5 +1,8 @@
 describe('Cherry Pick Tests', () => {
   beforeEach(() => {
+    // Set up intercept before navigation
+    cy.intercept('GET', '**/photos-staging/_design/photos/_view/photo_ids*').as('photoIds')
+    
     // Login first
     cy.visit('/login.php')
     cy.get('input[name="uname"]').type('joe')
@@ -7,12 +10,59 @@ describe('Cherry Pick Tests', () => {
     cy.get('form').submit()
   })
 
-  it('should load the main page and check an image', () => {
+  it('should select and deselect all images using select-all checkbox', () => {
     cy.url().should('include', 'index.php')
     cy.wait(500)
     
-    // Intercept the CouchDB request
-    cy.intercept('GET', '**/photos-staging/_design/photos/_view/photo_ids*').as('photoIds')
+    // Get the iframe
+    cy.get('iframe[src*="imgArrayTbl.php"]')
+      .should('be.visible')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap)
+      .within(() => {
+        // Wait for CouchDB request
+        cy.wait('@photoIds')
+        cy.wait(500)
+        
+        // Check select-all checkbox
+        cy.get('#selectAllCheckbox')
+          .should('exist')
+          .check({ force: true })
+          .should('be.checked')
+        cy.wait(500)
+
+        // Verify all checkboxes are checked
+        cy.get('input[type="checkbox"]').each(($checkbox) => {
+          if ($checkbox.attr('id') !== 'selectAllCheckbox') {
+            const $img = $checkbox.parent().next('img')
+            if ($img && $img.attr('data-objid') !== 'null') {
+              cy.wrap($checkbox).should('be.checked')
+            }
+          }
+        })
+
+        // Uncheck select-all checkbox
+        cy.get('#selectAllCheckbox')
+          .uncheck({ force: true })
+          .should('not.be.checked')
+        cy.wait(500)
+
+        // Verify all checkboxes are unchecked
+        cy.get('input[type="checkbox"]').each(($checkbox) => {
+          if ($checkbox.attr('id') !== 'selectAllCheckbox') {
+            const $img = $checkbox.parent().next('img')
+            if ($img && $img.attr('data-objid') !== 'null') {
+              cy.wrap($checkbox).should('not.be.checked')
+            }
+          }
+        })
+      })
+  })
+
+  it('should load the main page and check an image', () => {
+    cy.url().should('include', 'index.php')
+    cy.wait(500)
     
     // Get the iframe
     cy.get('iframe[src*="imgArrayTbl.php"]')
@@ -270,11 +320,177 @@ describe('Cherry Pick Tests', () => {
     cy.get('#return').should('be.visible') // Wait for page load
     cy.get('img[src="img/download.png"]').click()
 
-    cy.wait(5000)
+    cy.wait(500)
     
     // After download completes, verify we're back on image_info with state preserved
     cy.url().should('include', 'image_info.php')
     cy.url().should('include', 'tags=cherry')
     cy.url().should('include', 'checked=500b6984d64439bab876480128bdb420')
+  })
+
+  it('should search for images with produce tag, then exclude cherry', () => {
+    cy.url().should('include', 'index.php')
+    cy.wait(500)
+    
+    // Wait for the iframe to load
+    cy.get('iframe[src*="imgArrayTbl.php"]')
+      .should('be.visible')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap)
+      .within(() => {
+        cy.wait(500)
+        // Type 'produce' into the tag input and click search
+        cy.get('#tagInput')
+          .should('be.visible')
+          .type('produce')
+        cy.wait(1500)
+        
+        cy.get('#findImagesButton')
+          .should('be.visible')
+          .click()
+        cy.wait(500)
+      })
+
+    // Wait for the iframe to load, then check what's there, then exclude cherry
+    cy.get('iframe[src*="imgArrayTbl.php"]')
+      .should('be.visible')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap)
+      .within(() => {
+        // Check the input value
+        cy.get('#tagList')
+          .should('have.value', 'produce')
+          .should('have.attr', 'readonly')
+        cy.wait(500)
+        
+        // Wait for search results and verify the specific image is present
+        cy.get('img[data-objid="500b6984d64439bab876480128bdb420"]')
+          .should('exist')
+	  
+        cy.wait(500)
+	  
+        cy.get('img[data-objid="1ceb66fc7c47d0fc91e9e21b541ed8d7"]')
+          .should('exist')
+	  
+        cy.wait(500)
+
+        // Type 'cherry' into the tag input and click search
+        cy.get('#excludeTagInput')
+          .should('be.visible')
+          .type('cherry')
+        cy.wait(1500)
+        
+          cy.get('#excludeImagesButton')
+          .should('be.visible')
+          .click()
+        cy.wait(500)
+      })
+      
+    // Wait for the iframe to load, then check what's there
+    cy.get('iframe[src*="imgArrayTbl.php"]')
+      .should('be.visible')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap)
+      .within(() => {
+        // Check the input value
+        cy.get('#tagList')
+          .should('have.value', 'produce NOT cherry')
+          .should('have.attr', 'readonly')
+        cy.wait(500)
+	  
+        // Verify the cherry isn't present
+        cy.get('img[data-objid="500b6984d64439bab876480128bdb420"]')
+          .should('not.exist')
+	  
+        cy.wait(500)
+	  
+        // Verify the pepper image is present
+        cy.get('img[data-objid="1ceb66fc7c47d0fc91e9e21b541ed8d7"]')
+          .should('exist')
+	  
+        cy.wait(500)
+      })
+      
+  })
+
+    
+  it('should preserve selected and excluded state when downloading an image', () => {
+    cy.visit('/index.php')
+    cy.wait(500)
+
+    // Wait for the iframe to load
+    cy.get('iframe[src*="imgArrayTbl.php"]')
+      .should('be.visible')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap)
+      .within(() => {
+        // Type 'fruit' into the tag input and click search
+        cy.get('#tagInput')
+          .should('be.visible')
+          .type('produce')
+        cy.wait(500)
+        
+        cy.get('#findImagesButton')
+          .should('be.visible')
+          .click()
+        cy.wait(500)
+      })
+
+    // Check expected images, then add exlusion
+    cy.get('iframe[src*="imgArrayTbl.php"]')
+      .should('be.visible')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap)
+      .within(() => {
+        // check for cherry
+        cy.get('img[data-objid="500b6984d64439bab876480128bdb420"]')
+          .should('exist')
+
+        // check for pepper
+        cy.get('img[data-objid="1ceb66fc7c47d0fc91e9e21b541ed8d7"]')
+        .should('exist')
+
+        cy.wait(500)
+
+        // Type 'cherry' into the tag input and click search
+        cy.get('#excludeTagInput')
+          .should('be.visible')
+          .type('cherry')
+        cy.wait(1500)
+        
+          cy.get('#excludeImagesButton')
+          .should('be.visible')
+          .click()
+        cy.wait(500)
+      })
+
+    // Click on the image to go to image_info
+    cy.get('iframe[src*="imgArrayTbl.php"]')
+      .should('be.visible')
+      .its('0.contentDocument.body')
+      .should('not.be.empty')
+      .then(cy.wrap)
+      .within(() => {
+        cy.get('img[data-objid="1ceb66fc7c47d0fc91e9e21b541ed8d7"]')
+          .click()
+      })
+
+    // Verify URL contains our state
+    cy.url().should('include', 'tags=produce%20NOT%20cherry')
+
+    // Click download
+    cy.get('#return').should('be.visible') // Wait for page load
+    cy.get('img[src="img/download.png"]').click()
+
+    cy.wait(500)
+    
+    // After download completes, verify we're back on image_info with state preserved
+    cy.url().should('include', 'image_info.php')
+    cy.url().should('include', 'tags=produce%20NOT%20cherry')
   })
 })
