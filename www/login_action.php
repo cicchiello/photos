7c1718d6d3d644158bcb245342c3fe6e76312b7f
Joseph Cicchiello <joe@Eniac.local>
2026-04-28 16:11:20 -0400

    - fixing username-code-injection security risk - removing CouchDb password from config.ini (and recycling it manually)

--- www/login_action.php
+++ www/login_action.php
@@ -23,7 +23,7 @@
       $Db = $ini["dbname"];
       $DbViewBase = $DbBase.'/'.$Db.'/_design/photos/_view';
 
-      $usersUrl = $DbViewBase.'/users?key="user:'.$_POST['uname'].'"';
+      $usersUrl = $DbViewBase.'/users?key='.urlencode(json_encode('user:'.$_POST['uname']));
       $user_detail = json_decode(file_get_contents($usersUrl), true);
       $success = 0;
       if (count($user_detail['rows']) > 0)
